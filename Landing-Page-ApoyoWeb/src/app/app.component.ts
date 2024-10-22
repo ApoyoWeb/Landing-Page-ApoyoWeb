5c80261c6f88b92dee1880294734c065b86cceee
DeusExMachina203 <ernestodeloayza@gmail.com>
2024-10-21 20:26:40 -0500

    imagenes en carpeta

--- Landing-Page-ApoyoWeb/src/app/app.component.ts
+++ Landing-Page-ApoyoWeb/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { ReviewsComponent } from './reviews/reviews.component';
 import { BenefitsComponent } from './benefits/benefits.component';
 import { AboutComponent } from './about/about.component';
 import { CasesComponent } from './cases/cases.component';
+import { FaqComponent } from './faq/faq.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, ConturiasComponent, ReviewsComponent, BenefitsComponent, AboutComponent, CasesComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ConturiasComponent, ReviewsComponent, BenefitsComponent, AboutComponent, CasesComponent, FaqComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
